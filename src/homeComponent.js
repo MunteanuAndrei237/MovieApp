e11bac36cbee13b66604d796a34b8cbbdb361ea1
MunteanuAndrei237 <andreimunti9@gmail.com>
2023-12-05 23:11:47 +0200

    Added movie details

--- src/homeComponent.js
+++ src/homeComponent.js
@@ -1,24 +1,38 @@
 import { useEffect } from "react";
-import {fetchHomeMoviesThunk} from './slices/homeSlice.js';
-import { useSelector , useDispatch } from "react-redux";
-const HomeComponent =()=> {
+import { fetchHomeMoviesThunk , fetchMoovieDetailsByIdThunk } from './slices/homeSlice.js';
+import { useSelector, useDispatch } from "react-redux";
+const HomeComponent = () => {
 
-    const dispatch= useDispatch();
-useEffect(() => {
-dispatch(fetchHomeMoviesThunk())
-}, []);
-    const homeMoviesState= useSelector(state => state.home);
-    const homeMovies= useSelector(state => state.home.homeMovies);
-    console.log(homeMoviesState.homeMovies)
-   return (
-    homeMoviesState.status === 'loading' ? <p>loading...</p> : homeMoviesState.status === 'error' ? <div><h1>We ecnountered an error</h1><p>homeMoviesState.error </p></div> : 
-    homeMovies.map((movie) => {
-        return <p key={movie.id}>{movie.id}</p>
-    }
-                    
-   )
-   )
+  const dispatch = useDispatch();
+  const homeMoviesState = useSelector(state => state.home);
+
+  useEffect(() => {
     
+      dispatch(fetchHomeMoviesThunk());
+    
+  }, [dispatch]);
+
+  useEffect(() => {
+    
+    if (homeMoviesState.status === "succeeded") {
+      homeMoviesState.homeMovies.forEach(movie => {
+        dispatch(fetchMoovieDetailsByIdThunk({ id: movie.id, index: movie.index }));
+
+      });
+    }
+  }, [dispatch, homeMoviesState.status ]);
+
+  return (
+    homeMoviesState.status === 'loading' ? <p>loading...</p> : homeMoviesState.status === 'error' ? <div><h1>We ecnountered an error</h1><p>homeMoviesState.error </p></div> :
+      homeMoviesState.homeMovies.map((movie) => {
+        return <p key={movie.id}>
+          {movie.detailsFetched === true ? <p>{movie.title}</p> : <p>loading...</p>}
+          {movie.id}</p>
+      }
+
+      )
+  )
+
 }
 
 export default HomeComponent;
--- src/slices/homeSlice.js
+++ src/slices/homeSlice.js
@@ -1,6 +1,7 @@
 import {createSlice , createAsyncThunk } from '@reduxjs/toolkit';
 const fetch = require('node-fetch');
 const homeMoviesUrl = 'https://api.themoviedb.org/3/movie/popular';
+const moovieDetailsUrl = 'https://api.themoviedb.org/3/movie/';
 const options = {
   method: 'GET',
   headers: {
@@ -8,14 +9,36 @@ const options = {
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NGQ3NGQ0NTRmOTVmOTU3MDliY2QyOWRhMzVkOWE1NyIsInN1YiI6IjY1NmRlMzQ1NGE0YmY2MDEzZDhmNDNkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.psRlcMIT2Qtn0VesRX43LYqfEE-vFLD7JhPwN4W9uHc'
   }
 };
+let indexMovies = -1
+const fetchMoovieDetailsByIdThunk = createAsyncThunk('home/fetchMoovieDetailsById', async ({id,index}) => {
+    try {
+      const response = await fetch(moovieDetailsUrl + id, options);
+      const data = await response.json();
+      console.log('data:', data)
+      return { ...data, index: index,detailsFetched:true };
+    } catch (error) {
+      console.error('error:', error);
+      throw error; 
+    }
+  });
 
 const fetchHomeMoviesThunk = createAsyncThunk('home/fetchHomeMovies', async () =>
 {
-    let myres;
-    await fetch(homeMoviesUrl, options)
-    .then(res => myres= res.json())
-    .catch(err => console.error('error:' + err));
-    return myres;
+    try {
+        const response = await fetch(homeMoviesUrl, options);
+        const data = await response.json();
+        const newData = data.results.map((obj) => 
+        ({
+            ...obj,
+            imageFetched: false,
+            detailsFetched: false,
+            index: ++indexMovies,
+          }));
+        return newData;
+      } catch (error) {
+        console.error('error:', error);
+
+      }
 }
 )
 
@@ -35,14 +58,17 @@ const homeSlice = createSlice({
         });
         builder.addCase(fetchHomeMoviesThunk.fulfilled, (state, action) => {
             state.status = 'succeeded';
-            state.homeMovies = action.payload.results;
+            state.homeMovies = action.payload;
         });
         builder.addCase(fetchHomeMoviesThunk.rejected, (state) => {
             state.status = 'failed';
             state.error = 'We encountered an error';
         });
+        builder.addCase(fetchMoovieDetailsByIdThunk.fulfilled, (state, action) => {
+            state.homeMovies[Number(action.payload.index)]={...state.homeMovies[Number(action.payload.index)],...action.payload}
+        });
     }
 });
 
-export {fetchHomeMoviesThunk};
+export {fetchHomeMoviesThunk , fetchMoovieDetailsByIdThunk};
 export default homeSlice.reducer;
